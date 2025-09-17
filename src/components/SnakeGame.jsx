bd5664894256e233cf9b6dd3400ad78407fa4a0e
Scott Li <vikash23galce@gmail.com>
2025-09-17 15:39:41 -0600

    Refactor SnakeGame with react hooks

--- src/components/SnakeGame.jsx
+++ src/components/SnakeGame.jsx
@@ -17,190 +17,224 @@ import {
 
 // type MyProps = {};
 // type MyState = any;
-export default class SnakeGame extends Component {
-  constructor() {
-    super();
-    this.state = initialState;
-    this.nextDirection = this.state.direction;
+export default function SnakeGame() {
+  // constructor() {
+  //   super();
+  //   this.state = initialState;
+  //   this.nextDirection = this.state.direction;
 
-    // Request Animation Frame Tick Rate Update
-    this.lastUpdate = 0;
-    this.tickRate = this.state.speed;
-  }
+  //   // Request Animation Frame Tick Rate Update
+  //   this.lastUpdate = 0;
+  //   this.tickRate = this.state.speed;
+  // }
 
-  componentDidMount() {
-    document.onkeydown = this.onKeyDown;
-    this.rafId = requestAnimationFrame(this.gameLoop);
-  }
+  // componentDidMount() {
+  //   document.onkeydown = this.onKeyDown;
+  //   this.rafId = requestAnimationFrame(this.gameLoop);
+  // }
 
-  componentDidUpdate(prevProps, prevState) {
-    this.onSnakeOutOfBounds();
-    this.onSnakeCollapsed();
-    this.onSnakeEats();
-  }
+  // componentDidUpdate(prevProps, prevState) {
+  //   this.onSnakeOutOfBounds();
+  //   this.onSnakeCollapsed();
+  //   this.onSnakeEats();
+  // }
 
-  componentWillUnmount() {
-    cancelAnimationFrame(this.rafId);
-  }
+  // componentWillUnmount() {
+  //   cancelAnimationFrame(this.rafId);
+  // }
+  const [snakeDots, setSnakeDots] = useState(initialState.snakeDots);
+  const [direction, setDirection] = useState(initialState.direction);
+  const [food, setFood] = useState(initialState.food);
+  const [color, setColor] = useState(initialState.color);
+  const [route, setRoute] = useState(initialState.route);
+  const [score, setScore] = useState(initialState.score);
+  const [highScore, setHighScore] = useState(initialState.highScore);
+  const nextDirection = useRef(initialState.direction);
+  const tickRate = useRef(initialState.speed);
+  const lastUpdate = useRef(0);
+  const rafId = useRef(null);
 
-  onKeyDown = (e) => {
-    e.preventDefault();
-    e = e || window.event;
+  useEffect(() => {
+    const onKeyDown = (e) => {
+      e.preventDefault();
+      e = e || window.event;
 
-    switch (e.key) {
-      case "ArrowLeft":
-      case "A":
-      case "a":
-        if (this.state.direction !== "RIGHT") this.nextDirection = "LEFT";
-        break;
-      case "ArrowDown":
-      case "S":
-      case "s":
-        if (this.state.direction !== "UP") this.nextDirection = "DOWN";
-        break;
-      case "ArrowUp":
-      case "W":
-      case "w":
-        if (this.state.direction !== "DOWN") this.nextDirection = "UP";
-        break;
-      case "ArrowRight":
-      case "D":
-      case "d":
-        if (this.state.direction !== "LEFT") this.nextDirection = "RIGHT";
-        break;
-      case "P":
-      case "p":
-        this.setState({
-          route: this.state.route === "game" ? "pause" : "game",
-        });
-        break;
-    }
-  };
-
-  gameLoop = (timestamp) => {
-    if (!this.lastUpdate) {
-      this.lastUpdate = timestamp;
-    }
-    let elapsed = timestamp - this.lastUpdate;
-
-    if (elapsed > this.tickRate) {
-      if (this.state.route === "game") {
-        this.moveSnake();
-        elapsed -= this.tickRate;
-        this.lastUpdate = timestamp;
+      switch (e.key) {
+        case "ArrowLeft":
+        case "A":
+        case "a":
+          if (direction !== "RIGHT") nextDirection.current = "LEFT";
+          break;
+        case "ArrowDown":
+        case "S":
+        case "s":
+          if (direction !== "UP") nextDirection.current = "DOWN";
+          break;
+        case "ArrowUp":
+        case "W":
+        case "w":
+          if (direction !== "DOWN") nextDirection.current = "UP";
+          break;
+        case "ArrowRight":
+        case "D":
+        case "d":
+          if (direction !== "LEFT") nextDirection.current = "RIGHT";
+          break;
+        case "P":
+        case "p":
+          setRoute((prev) => (prev === "game" ? "pause" : "game"));
+          break;
       }
-    }
+    };
+    document.addEventListener("keydown", onKeyDown);
+    return () => document.removeEventListener("keydown", onKeyDown);
+  }, [direction]);
 
-    this.rafId = requestAnimationFrame(this.gameLoop);
-  };
+  useEffect(() => {
+    const gameLoop = (timestamp) => {
+      if (!lastUpdate.current) lastUpdate.current = timestamp;
+      let elapsed = timestamp - lastUpdate.current;
 
-  moveSnake = () => {
-    let body = [...this.state.snakeDots];
-    let head = body[body.length - 1];
-    if (this.state.route === "game") {
-      this.setState({ direction: this.nextDirection });
-      switch (this.state.direction) {
+      if (elapsed > tickRate.current && route === "game") {
+        moveSnake();
+        elapsed -= tickRate.current;
+        lastUpdate.current = timestamp;
+      }
+
+      rafId.current = requestAnimationFrame(gameLoop);
+    };
+
+    rafId.current = requestAnimationFrame(gameLoop);
+    return () => cancelAnimationFrame(rafId.current);
+  }, [route, snakeDots]); // rerun if route or snakeDots change
+
+  // gameLoop = (timestamp) => {
+  //   if (!this.lastUpdate) {
+  //     this.lastUpdate = timestamp;
+  //   }
+  //   let elapsed = timestamp - this.lastUpdate;
+
+  //   if (elapsed > this.tickRate) {
+  //     if (this.state.route === "game") {
+  //       this.moveSnake();
+  //       elapsed -= this.tickRate;
+  //       this.lastUpdate = timestamp;
+  //     }
+  //   }
+
+  //   this.rafId = requestAnimationFrame(this.gameLoop);
+  // };
+
+  // moveSnake = () => {
+  //   let body = [...this.state.snakeDots];
+  //   let head = body[body.length - 1];
+  //   if (this.state.route === "game") {
+  //     this.setState({ direction: this.nextDirection });
+  //     switch (this.state.direction) {
+  //       case "LEFT":
+  //         head = [head[0] - 2, head[1]];
+  //         break;
+  //       case "RIGHT":
+  //         head = [head[0] + 2, head[1]];
+  //         break;
+  //       case "UP":
+  //         head = [head[0], head[1] - 2];
+  //         break;
+  //       case "DOWN":
+  //         head = [head[0], head[1] + 2];
+  //         break;
+  //     }
+  //     body.push(head);
+  //     body.shift();
+  //     this.setState({ snakeDots: body })
+  //   }
+  // };
+  const moveSnake = () => {
+    setSnakeDots((prevDots) => {
+      const newDots = [...prevDots];
+      console.log(newDots);
+      const head = [...newDots[newDots.length - 1]];
+      const dir = nextDirection.current;
+
+      setDirection(dir);
+
+      switch (dir) {
         case "LEFT":
-          head = [head[0] - 2, head[1]];
+          head[0] -= 2;
           break;
         case "RIGHT":
-          head = [head[0] + 2, head[1]];
+          head[0] += 2;
           break;
         case "UP":
-          head = [head[0], head[1] - 2];
+          head[1] -= 2;
           break;
         case "DOWN":
-          head = [head[0], head[1] + 2];
+          head[1] += 2;
           break;
       }
-      body.push(head);
-      body.shift();
-      this.setState({ snakeDots: body });
-    }
+
+      newDots.push(head);
+      newDots.shift();
+
+      // Collisions
+      if (
+        route === "game" &&
+        (onSnakeOutOfBounds(head) || onSnakeCollapsed(head, newDots))
+      ) {
+        gameOver();
+      }
+
+      // Eating food
+      if (head[0] == food[0] && head[1] == food[1]) {
+        setFood(getRandomFood());
+        setScore(score + 1);
+        increaseSnake(newDots);
+        increaseSpeed(newDots.length);
+        changeColor();
+      }
+
+      return newDots;
+    });
   };
 
-  onSnakeOutOfBounds() {
-    let head = this.state.snakeDots[this.state.snakeDots.length - 1];
-    if (this.state.route === "game") {
-      if (
-        head[0] >= GRID_SIZE ||
-        head[1] >= GRID_SIZE ||
-        head[0] < 0 ||
-        head[1] < 0
-      ) {
-        this.gameOver();
-      }
-    }
-  }
+  const onSnakeOutOfBounds = (head) => {
+    return (
+      head[0] >= GRID_SIZE || head[1] >= GRID_SIZE || head[0] < 0 || head[1] < 0
+    );
+  };
 
-  onSnakeCollapsed() {
-    let body = [...this.state.snakeDots];
-    let head = body[body.length - 1];
-
+  const onSnakeCollapsed = (head, newDots) => {
+    var body = [...newDots];
     body.pop();
     body.forEach((dot) => {
-      if (dot[0] == head[0] && dot[1] == head[1]) {
-        this.gameOver();
-      }
+      return dot[0] == head[0] && dot[1] == head[1];
     });
-  }
+  };
 
-  onSnakeEats() {
-    let head = this.state.snakeDots[this.state.snakeDots.length - 1];
-    let food = this.state.food;
+  const changeColor = () => {
+    setColor(colorList[(food[0] + food[1]) % colorList.length]);
+  };
 
-    if (head[0] == food[0] && head[1] == food[1]) {
-      this.setState({
-        food: getRandomFood(),
-        score: this.state.score + 1,
-      });
-      this.increaseSnake();
-      this.increaseSpeed();
-      this.changeColor();
-    }
-  }
+  const increaseSnake = (newDots) => {
+    newDots.unshift([]);
+  };
 
-  changeColor() {
-    this.setState({
-      color:
-        colorList[(this.state.food[0] + this.state.food[1]) % colorList.length],
-    });
-  }
-
-  increaseSnake() {
-    let newSnake = [...this.state.snakeDots];
-    newSnake.unshift([]);
-    this.setState({
-      snakeDots: newSnake,
-    });
-  }
-
-  increaseSpeed() {
-    if (this.tickRate > 10) {
+  const increaseSpeed = (length) => {
+    if (tickRate.current > 10) {
       // console.log(
-      //   this.tickRate,
+      //   tickRate.current,
       //   Math.log10(this.state.snakeDots.length).toFixed(2)
       // );
-      this.tickRate =
-        this.tickRate - Math.log10(this.state.snakeDots.length).toFixed(2);
-    }
-  }
-
-  onRouteChange = () => {
-    if (this.state.route === "menu") {
-      this.setState({
-        route: "game",
-      });
-    }
-    if (this.state.route === "pause") {
-      this.setState({
-        route: "game",
-      });
+      tickRate.current = tickRate.current - Math.log10(length).toFixed(2);
     }
   };
 
-  gameOver() {
-    var newHighScore = Math.max(this.state.score, this.state.highScore);
+  const onRouteChange = () => {
+    if (route === "menu" || route === "pause") setRoute("game");
+  };
+
+  const gameOver = () => {
+    var newHighScore = Math.max(score, highScore);
 
     localStorage.setItem(
       ALL_TIME_HIGH_SCORE_KEY,
@@ -208,105 +242,103 @@ export default class SnakeGame extends Component {
     );
 
     alert(
-      `GAME OVER, your score is ${
-        this.state.score
-      }, high score is ${newHighScore}, all time high score is ${localStorage.getItem(
+      `GAME OVER, your score is ${score}, high score is ${newHighScore}, all time high score is ${localStorage.getItem(
         ALL_TIME_HIGH_SCORE_KEY
       )}`
     );
 
-    this.setState({ ...initialState, highScore: newHighScore });
-
-    // reset next direction
-    this.nextDirection = "RIGHT";
-    // reset tick rate
-    this.tickRate = initialState.speed;
-  }
-
-  onDown = () => {
-    let dots = [...this.state.snakeDots];
-    let head = dots[dots.length - 1];
-
-    head = [head[0], head[1] + 2];
-    dots.push(head);
-    dots.shift();
-    this.setState({
-      direction: "DOWN",
-      snakeDots: dots,
-    });
+    // Reset state
+    setSnakeDots(initialState.snakeDots);
+    setDirection(initialState.direction);
+    setRoute("menu");
+    setFood(initialState.food);
+    setColor(initialState.color);
+    setHighScore(newHighScore);
+    tickRate.current = initialState.speed;
+    nextDirection.current = "RIGHT";
   };
 
-  onUp = () => {
-    let dots = [...this.state.snakeDots];
-    let head = dots[dots.length - 1];
+  // onDown = () => {
+  //   let dots = [...this.state.snakeDots];
+  //   let head = dots[dots.length - 1];
 
-    head = [head[0], head[1] - 2];
-    dots.push(head);
-    dots.shift();
-    this.setState({
-      direction: "UP",
-      snakeDots: dots,
-    });
-  };
+  //   head = [head[0], head[1] + 2];
+  //   dots.push(head);
+  //   dots.shift();
+  //   this.setState({
+  //     direction: "DOWN",
+  //     snakeDots: dots,
+  //   });
+  // };
 
-  onRight = () => {
-    let dots = [...this.state.snakeDots];
-    let head = dots[dots.length - 1];
+  // onUp = () => {
+  //   let dots = [...this.state.snakeDots];
+  //   let head = dots[dots.length - 1];
 
-    head = [head[0] + 2, head[1]];
-    dots.push(head);
-    dots.shift();
-    this.setState({
-      direction: "RIGHT",
-      snakeDots: dots,
-    });
-  };
+  //   head = [head[0], head[1] - 2];
+  //   dots.push(head);
+  //   dots.shift();
+  //   this.setState({
+  //     direction: "UP",
+  //     snakeDots: dots,
+  //   });
+  // };
 
-  onLeft = () => {
-    let dots = [...this.state.snakeDots];
-    let head = dots[dots.length - 1];
+  // onRight = () => {
+  //   let dots = [...this.state.snakeDots];
+  //   let head = dots[dots.length - 1];
 
-    head = [head[0] - 2, head[1]];
-    dots.push(head);
-    dots.shift();
-    this.setState({
-      direction: "LEFT",
-      snakeDots: dots,
-    });
-  };
+  //   head = [head[0] + 2, head[1]];
+  //   dots.push(head);
+  //   dots.shift();
+  //   this.setState({
+  //     direction: "RIGHT",
+  //     snakeDots: dots,
+  //   });
+  // };
 
-  render() {
-    const { route, snakeDots, food, color } = this.state;
+  // onLeft = () => {
+  //   let dots = [...this.state.snakeDots];
+  //   let head = dots[dots.length - 1];
 
-    return (
-      <div>
-        <p className={style.scoreboard}>
-          Score: {this.state.score}, HighScore: {this.state.highScore}, All
-          Time: {localStorage.getItem(ALL_TIME_HIGH_SCORE_KEY) || 0}
-        </p>
-        <div className={style.gameArea}>
-          {/* // <div className={style.gridLines}> */}
-          {route === "pause" ? (
-            <div>
-              <Pause onRouteChange={this.onRouteChange} />
-            </div>
-          ) : (
-            ""
-          )}
-          {route === "menu" ? (
-            <div>
-              <Menu onRouteChange={this.onRouteChange} />
-            </div>
-          ) : (
-            <div>
-              <Snake snakeDots={snakeDots} color={color} />
-              <Food dot={food} cellSize={CELL_SIZE} foodSize={FOOD_SIZE} />
-            </div>
-          )}
-        </div>
+  //   head = [head[0] - 2, head[1]];
+  //   dots.push(head);
+  //   dots.shift();
+  //   this.setState({
+  //     direction: "LEFT",
+  //     snakeDots: dots,
+  //   });
+  // };
+
+  // render() {
+  //   const { route, snakeDots, food, color } = this.state;
+
+  return (
+    <div>
+      <p className={style.scoreboard}>
+        Score: {score}, HighScore: {highScore}, All Time:{" "}
+        {localStorage.getItem(ALL_TIME_HIGH_SCORE_KEY) || 0}
+      </p>
+      <div className={style.gameArea}>
+        {/* // <div className={style.gridLines}> */}
+        {route === "pause" ? (
+          <div>
+            <Pause onRouteChange={onRouteChange} />
+          </div>
+        ) : (
+          ""
+        )}
+        {route === "menu" ? (
+          <div>
+            <Menu onRouteChange={onRouteChange} />
+          </div>
+        ) : (
+          <div>
+            <Snake snakeDots={snakeDots} color={color} />
+            <Food dot={food} cellSize={CELL_SIZE} foodSize={FOOD_SIZE} />
+          </div>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-// export default SnakeGame;
